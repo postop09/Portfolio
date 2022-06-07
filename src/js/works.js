550f619a091dba8e20991cf4904ad45081ffa0f7
CHO YUN SIK <postop09@naver.com>
2022-06-08 02:07:44 +0900

    [Feat] 모달창 추가
    1. 자세히 보기 모달창 (아이엠모모, 넷플릭스, 감귤마켓, 포트폴리오)
    - 닫기 버튼 또는 다른 view more 클릭 시 모달창 닫기
    
    2. 이미지 모달창 UI
    - Swiper.js 사용 (기본 슬라이드)
    - 이미지는 최대 3장 예정

--- src/js/works.js
+++ src/js/works.js
@@ -1,1 +1,51 @@
 // 작품
+function secWorks() {
+  const $wrapSecModal = document.querySelector('.wrap_secModal');
+  const $secModal = $wrapSecModal.querySelectorAll('.sec_modalDetail');
+  const [$secMomo, $secNetflix, $secMandarine, $secPortfolio] = [...$secModal];
+  const $btnViewMore = $wrapSecModal.querySelectorAll('.btn_viewMore');
+  const $btnClose = $wrapSecModal.querySelectorAll('.btn_close');
+  // 모달창 열고 닫기
+  [...$btnViewMore].map((btn) => {
+    btn.addEventListener('click', () => {
+      [...$secModal].map((sec) => {
+        sec.classList.remove('on');
+      })
+    })
+    if (btn.name === 'momo') {
+      btn.addEventListener('click', () => {
+        $secMomo.classList.add('on');
+      })
+    } else if (btn.name === 'netflix') {
+      btn.addEventListener('click', () => {
+        $secNetflix.classList.add('on');
+      })
+    } else if (btn.name === 'mandarine') {
+      btn.addEventListener('click', () => {
+        $secMandarine.classList.add('on');
+      })
+    } else if (btn.name === 'portfolio') {
+      btn.addEventListener('click', () => {
+        $secPortfolio.classList.add('on');
+      })
+    }
+  });
+  // 모달창 닫기
+  [...$btnClose].map((btn) => {
+    btn.addEventListener('click', (e) => {
+      let result = findNode(e.target, 'sec_modalDetail on');
+      if (result) {
+        result.classList.remove('on');
+      }
+    })
+  });
+}
+secWorks();
+
+// 노드 찾기
+function findNode(target, tName) {
+  while (target.className !== tName) {
+    target = target.parentNode;
+  }
+  return target;
+}

d145bd5a9f8bfcf51d90434293d7b5668e5f7856
조윤식 <postop@tuneit.io>
2023-09-13 23:48:05 +0900

    롤챔 프로젝트 js 코드 수정

--- src/js/works.js
+++ src/js/works.js
@@ -1,76 +1,97 @@
 // 내용 자세히 보기 모달창
-const $wrapSecModal = document.querySelector('.wrap_secModal');
+const $wrapSecModal = document.querySelector(".wrap_secModal");
 
 function detailModal() {
-  const $secModal = $wrapSecModal.querySelectorAll('.sec_modalDetail');
-  const [$secAllReborn, $secMomo, $secNetflix, $secMandarine, $secPortfolio] = [...$secModal];
-  const $btnViewMore = document.querySelectorAll('.btn_viewMore');
-  const $btnClose = $wrapSecModal.querySelectorAll('.btn_close');
+  const $secModal = $wrapSecModal.querySelectorAll(".sec_modalDetail");
+  const [
+    $secLolchamp,
+    $secAllReborn,
+    $secMomo,
+    $secNetflix,
+    $secMandarine,
+    $secPortfolio,
+  ] = [...$secModal];
+  const $btnViewMore = document.querySelectorAll(".btn_viewMore");
+  const $btnClose = $wrapSecModal.querySelectorAll(".btn_close");
   // 모달창 열고 닫기
   [...$btnViewMore].map((btn) => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener("click", () => {
       [...$secModal].map((sec) => {
-        sec.classList.remove('on');
-      })
-    })
-    btn.addEventListener('click', () => {
-      if (btn.name === 'all_reborn') {
-        $secAllReborn.classList.add('on');
-      } else if (btn.name === 'momo') {
-        $secMomo.classList.add('on');
-      } else if (btn.name === 'netflix') {
-        $secNetflix.classList.add('on');
-      } else if (btn.name === 'mandarine') {
-        $secMandarine.classList.add('on');
-      } else if (btn.name === 'portfolio') {
-        $secPortfolio.classList.add('on');
+        sec.classList.remove("on");
+      });
+    });
+    btn.addEventListener("click", () => {
+      if (btn.name === "lolchamp") {
+        $secLolchamp.classList.add("on");
+      } else if (btn.name === "all_reborn") {
+        $secAllReborn.classList.add("on");
+      } else if (btn.name === "momo") {
+        $secMomo.classList.add("on");
+      } else if (btn.name === "netflix") {
+        $secNetflix.classList.add("on");
+      } else if (btn.name === "mandarine") {
+        $secMandarine.classList.add("on");
+      } else if (btn.name === "portfolio") {
+        $secPortfolio.classList.add("on");
       }
-    })
+    });
   });
   // 모달창 닫기
   [...$btnClose].map((btn) => {
-    btn.addEventListener('click', (e) => {
-      let result = findNode(e.target, 'sec_modalDetail on');
+    btn.addEventListener("click", (e) => {
+      let result = findNode(e.target, "sec_modalDetail on");
       if (result) {
-        result.classList.remove('on');
+        result.classList.remove("on");
       }
-    })
+    });
   });
 }
 detailModal();
 
 // 이미지 모달창
 function imgModal() {
-  const $btnImg = document.querySelectorAll('.btn_imgWork');
-  const $secImgModal = $wrapSecModal.querySelectorAll('.sec_modalImg');
-  const [$secImgAllReborn, $secImgMomo, $secImgNetflix, $secImgMandarine, $secImgportfolio] = [...$secImgModal];
-  const $body = document.querySelector('body');
+  const $btnImg = document.querySelectorAll(".btn_imgWork");
+  const $secImgModal = $wrapSecModal.querySelectorAll(".sec_modalImg");
+  const [
+    $secLolchamp,
+    $secImgAllReborn,
+    $secImgMomo,
+    $secImgNetflix,
+    $secImgMandarine,
+    $secImgportfolio,
+  ] = [...$secImgModal];
+  const $body = document.querySelector("body");
 
   [...$btnImg].map((btn) => {
-    btn.addEventListener('click', () => {
-      $body.style.overflow = 'hidden';
-      if (btn.name === 'all_reborn') {
-        $secImgAllReborn.classList.add('on');
-      } else if (btn.name === 'momo') {
-        $secImgMomo.classList.add('on');
-      } else if (btn.name === 'netflix') {
-        $secImgNetflix.classList.add('on');
-      } else if (btn.name === 'mandarine') {
-        $secImgMandarine.classList.add('on');
-      } else if (btn.name === 'portfolio') {
-        $secImgportfolio.classList.add('on');
+    btn.addEventListener("click", () => {
+      $body.style.overflow = "hidden";
+      if (btn.name === "lolchamp") {
+        $secLolchamp.classList.add("on");
+      } else if (btn.name === "all_reborn") {
+        $secImgAllReborn.classList.add("on");
+      } else if (btn.name === "momo") {
+        $secImgMomo.classList.add("on");
+      } else if (btn.name === "netflix") {
+        $secImgNetflix.classList.add("on");
+      } else if (btn.name === "mandarine") {
+        $secImgMandarine.classList.add("on");
+      } else if (btn.name === "portfolio") {
+        $secImgportfolio.classList.add("on");
       }
-    })
+    });
   });
 
   [...$secImgModal].map((sec) => {
-    sec.addEventListener('click', (e) => {
-      if (e.target.className === 'sec_modalImg on' || e.target.className === 'img_imgClose') {
-        sec.classList.remove('on');
-        $body.style.overflow = 'auto';
+    sec.addEventListener("click", (e) => {
+      if (
+        e.target.className === "sec_modalImg on" ||
+        e.target.className === "img_imgClose"
+      ) {
+        sec.classList.remove("on");
+        $body.style.overflow = "auto";
       }
-    })
-  })
+    });
+  });
 }
 imgModal();
 
